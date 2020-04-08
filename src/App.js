f97e834e081e00cf01ab5d1df82ee2e383c7bedc
elvisplaza <elvis.plaza@hotmail.com>
2020-04-08 13:56:28 -0400

    cards are ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-
 // components
-
 import FlipCard from "./components/FlipCard/FlipCard";
 
+// data
+import { stockData } from "./data/stockData";
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <FlipCard />
+        {stockData.map((card) => {
+          return <FlipCard front={card.front} back={card.back} title={card.title} />;
+        })}
       </div>
     );
   }
---
+++ src/data/stockData.js
@@ -0,0 +1,45 @@
+export const stockData = [
+  {
+    front: "https://www.best-trading-platforms.com/sites/default/files/Chart-Pattern-1-2-3-4-Jeff-Cooper-No-Signal.png",
+    back:
+      "Strong stock only see weakness for a short period of time and then are ready to run up and move higher once again. Look for 3 day weakness, consecutively getting lower lows then that will trigger a third day high, and breakout",
+    title: "1234 Pattern",
+  },
+  {
+    front: "https://www.timothysykes.com/wp-content/uploads/2019/01/NBEV-flat-top-breakout-June-2018.png",
+    back: "when the price will breakout by moving above the top of the flat top pattern",
+    title: "Flat Top Breakout Pattern",
+  },
+  {
+    front: "https://a.c-dn.net/b/2oIN6k/bullish-flag-pattern_body_IDENTIFYINGFLAGOLE.png.full.png",
+    back:
+      "A bull flag pattern is a chart pattern that occurs when a stock is in a strong uptrend. It is called a flag pattern because when you see it on a chart it looks like a flag on a pole and since we are in an uptrend it is considered a bullish flag.",
+    title: "Bull Flag Pattern",
+  },
+  {
+    front: "https://www.investorsunderground.com/wp-content/uploads/2017/03/redgreenmove.jpg",
+    back:
+      "When a stock is trading below the previous day's closing price, it is considered to be “red,” whereas if it is trading above the previous day's closing price, it is considered to be “green.” When a stock goes from red to green, the share price moves from below the previous close to above the previous close.",
+    title: "Red to Green Move",
+  },
+  //  {
+  //   front:,
+  //   back:,
+  //   title:"",
+  // },
+  //  {
+  //   front:,
+  //   back:,
+  //   title:"",
+  // },
+  //  {
+  //   front:,
+  //   back:,
+  //   title:"",
+  // },
+  //  {
+  //   front:,
+  //   back:,
+  //   title:"",
+  // },
+];
