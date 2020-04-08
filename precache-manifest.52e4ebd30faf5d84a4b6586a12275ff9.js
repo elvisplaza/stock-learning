e72de9c6dface248edbb299c7a7c5d8e85e0b3bb
elvisplaza <elvis.plaza@hotmail.com>
2020-04-08 14:10:21 -0400

    Updates

---
+++ precache-manifest.52e4ebd30faf5d84a4b6586a12275ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b626f45b7dc539a2456149e332bd2c9",
+    "url": "/stock-learning/index.html"
+  },
+  {
+    "revision": "7c31ed5437f5bb5b9172",
+    "url": "/stock-learning/static/css/main.19e22664.chunk.css"
+  },
+  {
+    "revision": "49c2707e4fd258ad870a",
+    "url": "/stock-learning/static/js/2.47441644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-learning/static/js/2.47441644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c31ed5437f5bb5b9172",
+    "url": "/stock-learning/static/js/main.162d7574.chunk.js"
+  },
+  {
+    "revision": "0a33ff3f4e51901b7054",
+    "url": "/stock-learning/static/js/runtime-main.97f125d9.js"
+  }
+]);
